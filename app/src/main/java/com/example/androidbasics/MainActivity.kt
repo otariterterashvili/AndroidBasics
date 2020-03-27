2f907d4ba0a218b8aa067cef4b769efce6204719
otariterterashvili <otar.terterashvili.1@btu.edu.ge>
2020-03-27 22:06:54 +0400

    feat: task one

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.androidbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init(){
+        logInButton.setOnClickListener{
+            val email = userEmail.text.toString()
+            val password = userPassword.text.toString()
+            if(email.isNotEmpty() && password.isNotEmpty()){
+                Toast.makeText(this, "Log in success", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "Error field is empty", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
